add3b6843292e9a17249805be9d3f5053a90fa51
Dell <Dell@DESKTOP-MKT7G2T>
2022-06-09 13:52:13 +0530

    Added Day05 Contents

---
+++ Day05 Control Statements/src/InfiniteWhileLoop.java
@@ -0,0 +1,11 @@
+
+public class InfiniteWhileLoop {
+
+	public static void main(String[] args) {
+		while(true) { 
+		      System.out.println("infinitive while loop"); 
+		    } 
+
+	}
+
+}
---
+++ Day05 Control Statements/src/module-info.java
@@ -0,0 +1,2 @@
+module controlstatements {
+}
