ecf89b726c775f47d2a1338b90a3221220bd9b0c
Dell <Dell@DESKTOP-MKT7G2T>
2022-06-13 14:55:43 +0530

    Added Constructor

---
+++ Constructor/src/Main.java
@@ -0,0 +1,36 @@
+class Employee {
+int id;
+String name;
+float salary;
+Employee(int id, String name, float salary) {
+
+System.out.println("user defined no-argument "
+
++ "constructor executed");
+
+}
+
+void display() {
+
+System.out.println(id + " " + name + " " + salary);
+}
+}
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee(101, "ajeet", 45000);
+
+		Employee e2 = new Employee(102, "irfan", 25000);
+
+		Employee e3 = new Employee(103, "nakul", 55000);
+
+		e1.display();
+
+		e2.display();
+
+		e3.display();
+		}
+
+}
---
+++ Constructor/src/Main1.java
@@ -0,0 +1,37 @@
+class Employee {
+
+int id;
+
+String name;
+
+float salary;
+
+Employee() {
+
+System.out.println("user defined no-argument "
+
++ "constructor executed");
+
+}
+
+void display() {
+
+System.out.println(id + " " + name + " " + salary);
+
+}
+
+}
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee();
+
+		Employee e2 = new Employee();
+
+		e1.display();
+
+		e2.display();
+		}
+
+}
