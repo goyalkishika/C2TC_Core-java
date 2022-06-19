8e1dcf5a121e0f64df07b8a62ff82ba85dc61c42
Dell <Dell@192.168.0.197>
2022-06-19 14:37:59 +0530

    Added "Static" Keyword

---
+++ StaticKeyword/src/Stduent.java
@@ -0,0 +1,28 @@
+
+
+class Student 
+{
+	int rollno;
+	String name;
+	static String college= "ITS";
+	Student(int r, String n)
+	{
+		rollno = r;
+		name = n;
+	}
+	static void change() {
+		college = "BBDIT";
+	}
+	void display()
+	{
+		System.out.println(rollno+" "+name+" "+college);
+	}
+	public static void main(String[] args) {
+		Student s1 = new Student(111,"Karan");
+		Student s2 = new Student(222,"Aryan");
+		s1.display();
+		s2.display();
+	}
+	
+
+}
