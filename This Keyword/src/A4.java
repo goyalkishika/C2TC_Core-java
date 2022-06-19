c944363623177c2e08cbe1a6b2422421d1da7f8d
Dell <Dell@192.168.0.197>
2022-06-19 14:21:24 +0530

    Added "this" Keyword

---
+++ This Keyword/src/A4.java
@@ -0,0 +1,23 @@
+class B{
+	A4 obj;
+	B(A4 obj){
+		this.obj=obj;
+	}
+	void display() {
+		System.out.println(obj.data);
+	}
+}
+class A4 {
+	ont data=10;
+	A4(){
+		B b=new B(this);
+		b.display();
+	}
+}
+class Main{
+	public static void main(String[] args) {
+		A4 a= new A4();
+
+	}
+
+}
