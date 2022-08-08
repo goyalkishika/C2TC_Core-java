599630e7f38a4f64a4835094cbe1e78977790d90
Dell <Dell@DESKTOP-MKT7G2T>
2022-08-08 21:28:36 +0530

    added access modifiers

---
+++ Access Modifiers/src/access_modifiers_demo/test1.java
@@ -0,0 +1,14 @@
+package access_modifiers_demo;
+
+public class test1 {
+	
+	int i=10;
+	void show() {
+	
+	public static void main(String[] args) {
+		test1 t1=new test1();
+		t1.show();
+		System.out.println(t1.i);
+	}
+	}
+}
---
+++ Access Modifiers/src/demo1/test3.java
@@ -0,0 +1,11 @@
+package demo1;
+import access_modifiers_demo.test1;
+public class test3 extends test1{
+
+	public static void main(String[] args) {
+		test3 t3=new test3();
+		t3.show();
+		System.out.Println(t3.i);
+	}
+
+}
