3c65447f87fa572057a4a983df636143034b5e50
Dell <Dell@DESKTOP-MKT7G2T>
2022-08-09 06:09:31 +0530

    added strings

---
+++ Strings intro/src/www/acharya/strings/ex1.java
@@ -0,0 +1,15 @@
+package www.acharya.strings;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		String str = "Hello";
+        String str1 = "Hello";
+        String str2 = new String("Hello");
+        System.out.println(str == str1);
+        System.out.println(str == str2);
+
+
+	}
+
+}
