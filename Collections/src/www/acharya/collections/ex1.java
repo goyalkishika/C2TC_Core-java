a3e5e506a27b48f339a8bc3af5b7fe677585a96a
Dell <Dell@DESKTOP-MKT7G2T>
2022-08-09 12:21:29 +0530

    added collections

---
+++ Collections/src/www/acharya/collections/ex1.java
@@ -0,0 +1,14 @@
+package www.acharya.collections;
+ListIterator e= refvariable.listIterator();
+while(e.hasNext())
+         {
+                   System.out.println(e.next());
+         }
+while(e.hasPrevious())
+       {
+                   System.out.println(e.previous());
+       }
+
+
+
+}
