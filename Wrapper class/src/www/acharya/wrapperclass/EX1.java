18bf7914f511ccc60a36c6ee1fcbf279b3d2df4c
Dell <Dell@DESKTOP-MKT7G2T>
2022-08-09 06:11:32 +0530

    added wrapper class

---
+++ Wrapper class/src/www/acharya/wrapperclass/EX1.java
@@ -0,0 +1,11 @@
+package www.acharya.wrapperclass;
+
+public class EX1 {
+	public static void main(String args[])
+	{
+		int a= 20;
+		Integer i= Integer.valueOf(a);
+		Integer j=a;
+		System.out.println(a+" "+i+" "+j);
+}
+}
